73d184e863f41568aed9cbf3272569e209662413
srkamesh <166099383+srkamesh@users.noreply.github.com>
2025-03-30 17:45:14 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+function Navbar()
+{
+  return(
+    
+    <nav>
+
+      <h1> Perfumy</h1>
+      <ul>
+        <li><a> Home</a></li>
+        <li><a> About Us</a></li>
+        <li><a> Product </a></li>
+        <li><a> Contact Us </a></li>
+      </ul>
+    </nav>
+
+  )
+}
+
+export default Navbar
